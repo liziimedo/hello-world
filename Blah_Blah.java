9e6373671eb3790e4e048668dbd67c360b2d96b0
Lizi <li48113p@pace.edu>
2025-04-07 01:34:19 -0400

    oh lalal

---
+++ Blah_Blah.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Blah_Blah here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Blah_Blah
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private int y;
+
+    /**
+     * Constructor for objects of class Blah_Blah
+     */
+    public Blah_Blah()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
